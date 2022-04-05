5e39a93d5874c17c8f5af3df8d9842507561a4e2
Cristhiane Jacques <cjn_1707@hotmail.com>
2022-04-05 19:33:55 -0300

    Implementações finais: criação dos Reducers, Store, páginas de Login, Perfil e Registro, página inicial

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+
 const API_URL = "http://localhost:8080/api/auth/";
+
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
     username,
@@ -7,6 +9,7 @@ const register = (username, email, password) => {
     password,
   });
 };
+
 const login = (username, password) => {
   return axios
     .post(API_URL + "signin", {
@@ -17,12 +20,15 @@ const login = (username, password) => {
       if (response.data.accessToken) {
         localStorage.setItem("user", JSON.stringify(response.data));
       }
+
       return response.data;
     });
 };
+
 const logout = () => {
   localStorage.removeItem("user");
 };
+
 export default {
   register,
   login,
